8368ca02a6954b2599623b7c06a933cead5ba578
Marcela Acosta <mpab28@gmail.com>
2022-08-10 23:43:18 +0900

    Actualiza simulador de js

--- js/desafio1.js
+++
@@ -1,5 +0,0 @@
-let entrada = prompt("Escribe algo");
-while(entrada != "ESC" ){
-    alert("El usuario escribió " + entrada);
-    entrada = prompt("Escribe otra cosa");
-}
--- js/simulador.js
+++ js/simulador.js
@@ -1,3 +1,47 @@
+// Lista de transportadores
+const listaTransportadores = [
+    {
+        nombre: "Transportador de plástico",
+        precio: "$950",
+        material: "Plástico",
+    },
+    {
+        nombre: "Transportador mochila",
+        precio: "$1450",
+        material: "Tela",
+    },
+    {
+        nombre: "Transportador burbuja",
+        precio: "$1550",
+        material: "Plástico",
+    },
+]
+
+// Visualización y cálculo de precios finales
+
+let action = ""
+
+while (action != "CALCULAR"){
+    action = prompt("Ingresa VER para ver los precios sin impuestos o CALCULAR para calcular los precios finales")
+    switch (action){
+        case "VER":
+            verTransportador()
+            break;
+        case "CALCULAR":
+            alert("Vas a calcular los precios finales")
+            break;
+        default:
+            alert("Ingresa una acción válida")
+            break;
+    }
+}
+
+function verTransportador(){
+    listaTransportadores.forEach((transportador)=>{
+        alert(`Nombre: ${transportador.nombre} Precio: ${transportador.precio} Material: ${transportador.material}`)
+    })
+}
+
 // Transportador de plástico
 const suma  = (a,b) => a + b
 const resta = (a,b) => a - b
@@ -7,21 +51,21 @@ let precioPlastico  = parseFloat(prompt("Escribe el valor del transportador de p
 let descuentoPlastico = precioPlastico * 0.1;
 
 let precioFinalPlastico = resta(suma(precioPlastico, iva(precioPlastico)), descuentoPlastico) 
-console.log(precioFinalPlastico)
+alert(`El precio final del transportador de plástico es: $${precioFinalPlastico}`)
 
 // Transportador mochila
 let precioMochila  = parseFloat(prompt("Escribe el valor del transportador mochila"));
 let descuentoMochila = precioMochila * 0.1; 
 
 let precioFinalMochila = resta(suma(precioMochila, iva(precioMochila)), descuentoMochila) 
-console.log(precioFinalMochila)
+alert(`El precio final del transportador mochila es: $${precioFinalMochila}`)
 
 // Transportador burbuja
 let precioBurbuja  = parseFloat(prompt("Escribe el valor del transportador burbuja"));
 let descuentoBurbuja = precioBurbuja * 0.1; 
 
 let precioFinalBurbuja = resta(suma(precioBurbuja, iva(precioBurbuja)), descuentoBurbuja) 
-console.log(precioFinalBurbuja)
+alert(`El precio final del transportador burbuja es: $${precioFinalBurbuja}`)
 
 // Promoción llevando los 3 transportadores
 const sumaFinal  = (a,b,c) => a + b + c
@@ -30,9 +74,25 @@ const multiplicacion = (a,b) => a * b
 let descuentoMayor = multiplicacion(sumaFinal(descuentoPlastico, descuentoMochila, descuentoBurbuja), 2)
 
 let precioFinalTransportadores = resta(sumaFinal(precioFinalPlastico, precioFinalMochila, precioFinalBurbuja), descuentoMayor)
-console.log(precioFinalTransportadores)
 
-//Array que me muestra los productos que tengo
+let entrada = ""
+
+while (entrada != "NO"){
+    entrada = prompt("¿Quieres llevar los 3 transportadores? Ingresa NO para salir")
+    switch (entrada){
+        case "SI":
+            alert(`El precio final por los 3 transportadores es: $${precioFinalTransportadores}`)    
+            break;
+        case "NO":
+            alert("Muchas gracias por tu visita")
+            break;
+        default:
+            alert("Ingresa una acción válida: SI o NO")
+            break;
+    }
+}
+
+// Array que me muestra los productos que tengo
 const articulos = [{ id: 1, articulo: "Transportador de plástico" },
                   { id: 2,  articulo: "Transportador mochila" },
                   { id: 3,  articulo: "Transportador burbuja" }];
@@ -42,7 +102,7 @@ for (const articulo of articulos) {
     console.log(articulo.articulo);
 }
 
-//Array ajuste de precio (suma iva y resta descuento)
+// Array ajuste de precio (suma iva y resta descuento)
 class Producto {
     constructor(nombre, precio) {
         this.nombre  = nombre.toUpperCase();
@@ -53,12 +113,14 @@ class Producto {
         this.precio = this.precio * 1.22 - this.precio * 0.1;
     }
 }
-//Array de productos que almacena objetos
+
+// Array de productos que almacena objetos
 const productos = [];
 productos.push(new Producto("Transportador de plástico", "950"));
 productos.push(new Producto("Transportador mochila", "1450"));
 productos.push(new Producto("Transportador burbuja", "1550"));
-//Array con for...of para modificar todos los productos
+
+// Array con for...of para modificar todos los productos
 for (const producto of productos){
     producto.conIvaYdescuento();
 }
